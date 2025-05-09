8231af01a0cbcb7818460ea27d6fe5d1213b9507
joshuawester99 <Joshua_Wester1@baylor.edu>
2025-05-08 23:10:22 -0500

    Added comments

--- src/main/java/argon/kwic/App.java
+++ src/main/java/argon/kwic/App.java
@@ -18,11 +18,16 @@ public class App {
     	
     	readInputFile();
     	
+    	// for each line
     	for (String line : input) {
     		
-    		String[] words = line.split("\\W+"); 		
+    		// splits line into words 	
+    		String[] words = line.split("\\W+");	
     		
+    		// for every word
     		for (String word : words) {
+    			
+    			// add it to the map along with the corresponding shifted string
         		line = addCircularShiftedString(line);
     		}
     	}
@@ -30,27 +35,39 @@ public class App {
     	print_pairs();
     }
     
+    // used for reading the input file into a List<String>
     private static void readInputFile() throws IOException {
     	input = Files.readAllLines(Paths.get("input.txt"));
     }
     
+    // debugging method for checking state of the Map
     private static void print_pairs() {
     	for (String key : word_shift_pairs.keySet()) {
     		System.out.println(word_shift_pairs.get(key) + "\n");
     	}
     }
     
+    // removes first word from string and concatenates to the end, 
+    // returning the modified string
+    // violated Single-Responsibility principle but 
+    // each time the string is modified, it and its new first word are put in the map
     private static String addCircularShiftedString(String line) {
+    	// split into first word, any deliminator/break, and the rest of the string
     	String[] split = line.split("(?!^)\\b", 3); 
     	
+    	// trim each of excess
     	String first_word = split[0].trim();
     	String first_char = split[1].trim();
     	String remainder = split[2].trim();
+    	
+    	// get the first word of the new string
     	String remainder_key = remainder.split("(?!^)\\b", 2)[0].trim();
     	
+    	// perform the shift and put in the map
     	String shifted_string = remainder + " " + first_word + first_char;
     	word_shift_pairs.put(remainder_key, shifted_string);
     	
+    	// return the shifted string
     	return shifted_string;
     }
 }
